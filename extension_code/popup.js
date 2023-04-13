4180b3e3127c0327df5e39c8ede471e3f60ad6ac
amnaamin-20 <106994629+amnaamin-20@users.noreply.github.com>
2023-04-14 00:28:40 +0500

    Delete popup.js

--- extension_code/popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var changeColor = document.getElementById("changeColor");
-  changeColor.addEventListener("click", openIndex);
-});
-
-function openIndex() {
-  chrome.tabs.create({ active: true, url: "/interface/index.html" });
-}
